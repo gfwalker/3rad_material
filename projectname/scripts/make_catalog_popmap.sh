299b28e3f03ab21d2e603727fa8f5556316f4d13
gfwalker <walker.gf2@gmail.com>
2024-11-15 10:59:35 -0500

    Script updates/fixes, corresponding changes to walkthrough

--- projectname/scripts/make_catalog_popmap.sh
+++ projectname/scripts/make_catalog_popmap.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-cat schh_popmap.txt | grep GAL | shuf -n 20 > catalog_popmap.txt
-cat schh_popmap.txt | grep FLA | shuf -n 20 >> catalog_popmap.txt
-cat schh_popmap.txt | grep SEY | shuf -n 20 >> catalog_popmap.txt
-#awk '{print $1".1\t"$2}' subsample_popmap.txt > catalog_popmap.txt
+cat lib2_popmap.txt | grep GAL | shuf -n 20 > lib2_catalog_popmap.txt
+cat lib2_popmap.txt | grep FLA | shuf -n 20 >> lib2_catalog_popmap.txt
+cat lib2_popmap.txt | grep SEY | shuf -n 20 >> lib2_catalog_popmap.txt
